3dc60d0af08077a6e3da0ff28fcb9aa8d234d4d4
Dhanuka Rathnayaka <137430860+Dhanuka001@users.noreply.github.com>
2024-11-13 09:25:48 +0530

    style configurations

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -11,6 +11,22 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 1s ease-out forwards',
+      },
+      boxShadow: {
+        gradient: '0px 4px 20px rgba(72, 187, 255, 0.3), 0px 8px 40px rgba(0, 255, 191, 0.2)',
+      },
     },
   },
   plugins: [],
